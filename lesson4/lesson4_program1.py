d9ef3b84949607e439f9117c08cb12f23067b858
Aishwarya Sharma <ishans632@gmail.com>
2018-03-16 06:00:14 +0530

    Add lesson4_program1

---
+++ lesson4/lesson4_program1.py
@@ -0,0 +1,52 @@
+"""
+    udacity fsnd
+    lesson 4 practice problem
+    turtle graphics 
+
+"""
+
+
+import turtle
+
+def draw_square_circle():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    t = turtle.Turtle()
+    t.shape('turtle')
+    t.color('green')
+    t.speed(10)
+    for j in range(0,36):
+        t.right(10)
+        for i in range(4):  
+            t.forward(100)
+            t.right(90)
+
+
+    window.exitonclick()
+
+
+
+
+def wheel():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    t = turtle.Turtle()
+    t.shape('turtle')
+    t.color('green')
+    t.speed(10)
+    for j in range(0,36):
+        t.right(10)
+        for i in range(3):  
+            t.forward(100)
+            t.right(120)
+
+
+    window.exitonclick()
+    
+
+def 
+
+
+wheel()
