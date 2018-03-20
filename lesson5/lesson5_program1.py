49d006ac8fbae36a58088379b4badbe08c7c0b63
Aishwarya Sharma <ishans632@gmail.com>
2018-03-20 17:12:36 +0530

    Add lesson5_program2

---
+++ lesson5/lesson5_program1.py
@@ -0,0 +1,11 @@
+from twilio.rest import Client
+
+#Account Authorization and Auth Token from twilio.com/user/account
+account_sid = "*******************************"
+auth_token = "********************************"
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(body="Test messeage? Twilio client App",
+                                     to="+918*********",   # Receiver Number
+                                     from_="+12*******") # Twilio Number
+print(message.sid) 
